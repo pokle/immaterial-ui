3df5c9189468d018e633eb4973890829a2695eb6
Tushar Pokle <tushar.pokle@gmail.com>
2016-08-24 20:15:47 +1000

    Fixups

---
+++ script.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('ready', function () {
+    console.log("ready")
+    var header = document.getElementsByTagName('header')[0];
+    var search = document.getElementById('search')
+
+    search.addEventListener('focus', function (e) { header.style = 'background: #eee; color: black;' })
+    search.addEventListener('blur', function (e) { header.style = 'background: #4285f4; color: white;' })
+})
+
+
+function toggleSideNav() {
+    var sideNav = document.getElementById('sideNav');
+    if (sideNav.style.display == "none") {
+        sideNav.style.display = 'block';
+    } else {
+        sideNav.style.display = 'none';
+    }
+}
